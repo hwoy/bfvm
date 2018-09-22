d1d43baacf829958c68b0cc46f87a71b7f32ac8d
Watthanachai Dueanklang <bosskillerz@gmail.com>
2018-09-22 09:35:52 +0700

    modified:   include/main.hpp

--- include/main.hpp
+++ include/main.hpp
@@ -33,7 +33,7 @@ std::ofstream fout;
 
 if(argc >= 2)
 {
-	fout.open(argc==2 ? std::string(argv[1])+".bfc": std::string(argv[2]),std::ios::binary);
+	fout.open(argc==2 ? std::string(argv[1])+".ubc": std::string(argv[2]),std::ios::binary);
 	if(!fout) 
 	{
 		showerr(err_fout,err,(argc==2 ? std::string(argv[1])+".bfc": std::string(argv[2])).c_str());
