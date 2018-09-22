56621df49ad90a598077b543251fd74eff38d6bc
Watthanachai Dueanklang <bosskillerz@gmail.com>
2018-09-22 23:57:09 +0700

    On branch master

--- assfuck/config.hpp
+++
@@ -1,7 +0,0 @@
-#define PROGINST "ass","Ass","aSs","asS","ASs","aSS","AsS","ASS"
-
-#define PROGDESC "a aSSfuck esoteric to bytecode Compiler."
-
-#define PROGDESC2 "Bytecode to aSSfuck Decoder."
-
-
