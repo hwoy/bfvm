d81ef750199c5897d1b7e8977f1c86f266b38a95
Watthanachai Dueanklang <bosskillerz@gmail.com>
2018-09-27 20:45:14 +0700

    modified:   bfvm/main.cpp

--- bfvm/main.cpp
+++ bfvm/main.cpp
@@ -4,16 +4,12 @@
 
 #include <bfengine.hpp>
 #include <bytecode.hpp>
-#include <common.hpp>
+#include <help.hpp>
 #include "config.hpp"
 
 static void usage(const char *path,const char *str,std::size_t tapesize,std::size_t cellsize)
 {
-	auto gpath=path::grap(path);
-	
-	std::cerr << std::endl << gpath << " is " << str << std::endl << std::endl
-						  << gpath << " input-file" << std::endl
-						  << gpath << " input-file output-file" << std::endl << std::endl;
+	usage(path,str);
 	
 	std::cerr << "TAPESIZE = " << tapesize << ", CELLSIZE = " << cellsize*8 << " bits"<< std::endl << std::endl;
 	
